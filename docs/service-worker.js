d0d972f13d1b0a49aa91459cb727fb5e2f5ae41d
James Davies <james.davies@kineticdata.com>
2019-07-11 13:26:28 -0400

    0.1.0-alpha.2

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kineticdata/react-kinetic-lib/index.html", {
+workbox.routing.registerNavigationRoute("/react-kinetic-lib/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
