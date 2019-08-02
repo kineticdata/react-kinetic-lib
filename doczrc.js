95cb69c7be7224ed6cf5d3cd8a0d59541920b632
James Davies <james.davies@kineticdata.com>
2019-08-02 16:26:29 -0400

    Moved to docz v1

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,7 @@
 export default {
   title: 'Kinetic Reacat Components',
   dest: '/docs',
-  src: './src/components',
+  src: 'src',
+  wrapper: 'src/docz/wrapper',
   menu: ['Overview', 'Core', 'Discussions', 'Form', 'Table'],
 };
