c3ff2cc907b07b3433b40b7aaa57135634ad2042
James Davies <james.davies@kineticdata.com>
2019-08-06 18:06:38 -0400

    Updates

--- public/redirect.js
+++ public/redirect.js
@@ -1,7 +1,7 @@
 (function() {
   const redirect = sessionStorage.redirect;
   delete sessionStorage.redirect;
-  if (redirect && redirect !== location.href) {
-    history.replaceState(null, null, redirect);
+  if (redirect && redirect !== window.location.href) {
+    window.history.replaceState(null, null, redirect);
   }
 })();
