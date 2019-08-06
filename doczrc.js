2fb3cab48e1d0f1732733ca1ffb291949f717ae4
James Davies <james.davies@kineticdata.com>
2019-08-06 17:56:57 -0400

    Cleanup of documentation

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,11 @@ export default {
   title: 'Kinetic React Components',
   dest: '/docs',
   src: 'src',
+  files: '**/*.mdx',
+  indexHtml: 'src/docz/index.html',
+  public: 'src/docz/public',
   wrapper: 'src/docz/wrapper',
   menu: ['Overview', 'Core', 'Discussions', 'Form', 'Table'],
+  hashRouter: true,
+  base: '/react-kinetic-lib/',
 };
