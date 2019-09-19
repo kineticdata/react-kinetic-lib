57a7a4225999828f825323c9a1ba93ac5e4fb848
Shayne Koestler <shayne.koestler@kineticdata.com>
2019-09-19 13:39:03 -0500

    updates src/apis/core/bridgeModelAttributeMappings.js to use new apiFunction helper

--- src/apis/core/bridgeModelAttributeMappings.js
+++ src/apis/core/bridgeModelAttributeMappings.js
@@ -1,125 +1,55 @@
-import axios from 'axios';
-import { bundle } from '../../helpers';
-import {
-  handleErrors,
-  headerBuilder,
-  paramBuilder,
-  validateOptions,
-} from '../http';
+import { apiFunction } from '../http';
 
-export const fetchBridgeModelAttributeMappings = (options = {}) => {
-  validateOptions(
-    'fetchBridgeModelAttributeMappings',
-    ['modelName', 'mappingName'],
-    options,
-  );
-  return axios
-    .get(
-      `${bundle.apiLocation()}/models/${options.modelName}/mappings/${
-        options.mappingName
-      }/attributes`,
-      {
-        params: paramBuilder(options),
-        headers: headerBuilder(options),
-      },
-    )
-    .then(response => ({
-      bridgeModelAttributeMappings: response.data.attributes,
-    }))
-    .catch(handleErrors);
+const pluralApiConfig = {
+  requiredOptions: ['modelName', 'mappingName'],
+  url: ({ modelName, mappingName }) =>
+    `/models/${modelName}/mappings/${mappingName}/attributes`,
+  transform: response => ({
+    bridgeModelAttributeMappings: response.data.attributes,
+  }),
 };
 
-export const fetchBridgeModelAttributeMapping = (options = {}) => {
-  validateOptions(
-    'fetchBridgeModelAttributeMapping',
-    ['modelName', 'mappingName', 'attributeName'],
-    options,
-  );
-  const { modelName, mappingName, attributeName } = options;
-  return axios
-    .get(
-      `${bundle.apiLocation()}/models/${modelName}/mappings/${mappingName}/attributes/${attributeName}`,
-      {
-        params: paramBuilder(options),
-        headers: headerBuilder(options),
-      },
-    )
-    .then(response => ({
-      bridgeModelAttributeMapping: response.data.attribute,
-    }))
-    .catch(handleErrors);
+const singularApiConfig = {
+  requiredOptions: ['modelName', 'mappingName', 'attributeName'],
+  url: ({ modelName, mappingName, attributeName }) =>
+    `/models/${modelName}/mappings/${mappingName}/attributes/${attributeName}`,
+  transform: response => ({
+    bridgeModelAttributeMapping: response.data.attribute,
+  }),
 };
 
-export const createBridgeModelAttributeMapping = (options = {}) => {
-  validateOptions(
-    'createBridgeModelAttributeMapping',
-    ['modelName', 'mappingName', 'bridgeModelAttributeMapping'],
-    options,
-  );
-  return axios
-    .post(
-      `${bundle.apiLocation()}/models/${options.modelName}/mappings/${
-        options.mappingName
-      }/attributes`,
-      options.bridgeModelAttributeMapping,
-      {
-        params: paramBuilder(options),
-        headers: headerBuilder(options),
-      },
-    )
-    .then(response => ({
-      bridgeModelAttributeMapping: response.data.attribute,
-    }))
-    .catch(handleErrors);
-};
+const dataOption = 'bridgeModelAttributeMapping';
 
-export const updateBridgeModelAttributeMapping = (options = {}) => {
-  validateOptions(
-    'updateBridgeModelMapping',
-    [
-      'modelName',
-      'mappingName',
-      'attributeName',
-      'bridgeModelAttributeMapping',
-    ],
-    options,
-  );
+export const fetchBridgeModelAttributeMappings = apiFunction({
+  name: 'fetchBridgeModelAttributeMappings',
+  method: 'get',
+  ...pluralApiConfig,
+});
 
-  const {
-    modelName,
-    mappingName,
-    attributeName,
-    bridgeModelAttributeMapping,
-  } = options;
-  return axios
-    .put(
-      `${bundle.apiLocation()}/models/${modelName}/mappings/${mappingName}/attributes/${attributeName}`,
-      bridgeModelAttributeMapping,
-      { params: paramBuilder(options), headers: headerBuilder(options) },
-    )
-    .then(response => ({
-      bridgeModelAttributeMapping: response.data.attribute,
-    }))
-    .catch(handleErrors);
-};
+export const fetchBridgeModelAttributeMapping = apiFunction({
+  name: 'fetchBridgeModelAttributeMapping',
+  method: 'get',
+  ...singularApiConfig,
+});
 
-export const deleteBridgeModelAttributeMapping = (options = {}) => {
-  validateOptions(
-    'deleteBridgeModelAttributeMapping',
-    ['modelName', 'mappingName', 'attributeName'],
-    options,
-  );
-  const { modelName, mappingName, attributeName } = options;
-  return axios
-    .delete(
-      `${bundle.apiLocation()}/models/${modelName}/mappings/${mappingName}/attributes/${attributeName}`,
-      {
-        params: paramBuilder(options),
-        headers: headerBuilder(options),
-      },
-    )
-    .then(response => ({
-      bridgeModelAttribute: response.data.attribute,
-    }))
-    .catch(handleErrors);
-};
+export const createBridgeModelAttributeMapping = apiFunction({
+  name: 'createBridgeModelAttributeMapping',
+  method: 'post',
+  dataOption,
+  requiredOptions: pluralApiConfig.url,
+  transform: singularApiConfig.transform,
+  url: pluralApiConfig.url,
+});
+
+export const updateBridgeModelAttributeMapping = apiFunction({
+  name: 'updateBridgeModelAttributeMapping',
+  method: 'put',
+  dataOption,
+  ...singularApiConfig,
+});
+
+export const deleteBridgeModelAttributeMapping = apiFunction({
+  name: 'deleteBridgeModelAttributeMapping',
+  method: 'delete',
+  ...singularApiConfig,
+});
