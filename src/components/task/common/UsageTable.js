144469d366c6f1243fd5a89132915800bb8e0e1d
Thomas Roselyn <thomas.roselyn@kineticdata.com>
2019-10-11 13:35:21 -0500

    PUI-356: Consolidated UsageTable, table filter reset

---
+++ src/components/task/common/UsageTable.js
@@ -0,0 +1,64 @@
+import { generateTable } from '../../table/Table';
+import { fetchUsage } from '../../../apis';
+
+const dataSource = ({ definitionId, usageType }) => ({
+    fn: fetchUsage,
+    clientSideSearch: true,
+    params: () => [{ definitionId, usageType }],
+    transform: result => ({
+      data: result.usages,
+    }),
+  });
+
+const columns = [
+  {
+    value: 'type',
+    title: 'Type',
+    filter: 'equals',
+    type: 'text',
+    sortable: true,
+  },
+  {
+    value: 'source',
+    title: 'Source',
+    filter: 'startsWith',
+    type: 'text',
+    sortable: true,
+  },
+  {
+    value: 'group',
+    title: 'Group',
+    filter: 'startsWith',
+    type: 'text',
+    sortable: true,
+  },
+  {
+    value: 'name',
+    title: 'Tree',
+    filter: 'startsWith',
+    type: 'text',
+    sortable: true,
+  },
+  {
+    value: 'nodeCount',
+    title: 'Usage',
+    filter: 'startsWith',
+    type: 'text',
+    sortable: true,
+  },
+  {
+    value: 'updatedAt',
+    title: 'Updated',
+    filter: 'equals',
+    type: 'text',
+    sortable: true,
+  },
+];
+
+export const UsageTable = generateTable({
+  tableOptions: ['definitionId', 'usageType'],
+  columns,
+  dataSource,
+});
+
+UsageTable.displayName = 'UsageTable';
