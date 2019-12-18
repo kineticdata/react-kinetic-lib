553649fe4525ef3cd7f171fb445f2b918b863ecb
Shayne Koestler <shayne.koestler@kineticdata.com>
2019-12-18 12:34:34 -0600

    refactored BridgeForm to calculate adapterProperties in a datasource so alterFields in the PUI can use it PUI-624

--- src/components/agent/bridge/BridgeForm.js
+++ src/components/agent/bridge/BridgeForm.js
@@ -7,7 +7,7 @@ import {
   fetchAdapters,
 } from '../../../apis';
 
-const dataSources = ({ bridgeSlug, agentSlug }) => ({
+const dataSources = ({ bridgeSlug, agentSlug, adapterClass }) => ({
   bridge: {
     fn: fetchBridge,
     params: bridgeSlug && [{ agentSlug, bridgeSlug, include: 'details' }],
@@ -17,6 +17,19 @@ const dataSources = ({ bridgeSlug, agentSlug }) => ({
     fn: fetchAdapters,
     params: [{ include: 'details', type: 'bridge', agentSlug }],
     transform: result => result.adapters,
+  },
+  adapterProperties: {
+    fn: (adapters, bridge) => {
+      const appliedAdapterClass = bridge
+        ? bridge.get('adapterClass')
+        : adapterClass;
+      const adapter = adapters.find(
+        adapter => adapter.get('class') === appliedAdapterClass,
+      );
+      return adapter ? List(adapter.get('properties')) : List();
+    },
+    params: ({ adapters, bridge }) =>
+      (!bridgeSlug || bridge) && adapters && [adapters, bridge],
   },
 });
 
@@ -33,104 +46,95 @@ const handleSubmit = ({ bridgeSlug, agentSlug }) => values =>
     return bridge;
   });
 
-const fields = ({ bridgeSlug, adapterClass }) => ({ bridge, adapters }) => {
-  if (adapters && (!bridgeSlug || bridge)) {
-    const isNew = !bridge;
-    const appliedAdapterClass = bridge
-      ? bridge.get('adapterClass')
-      : adapterClass;
-    const adapter = adapters.find(
-      adapter => adapter.get('class') === appliedAdapterClass,
-    );
-    const adapterProperties = adapter
-      ? List(adapter.get('properties'))
-      : List();
-    return [
-      {
-        name: 'slug',
-        label: 'Bridge Slug',
-        type: 'text',
-        required: true,
-        initialValue: get(bridge, 'slug', ''),
-        helpText: 'Unique name used in the bridge path.',
-      },
-      {
-        name: 'adapterClass',
-        label: 'Adapter Class',
-        type: 'text',
-        enabled: false,
-        required: false,
-        initialValue: appliedAdapterClass,
-        options: adapters.map(adapter =>
-          Map({
-            value: adapter.get('class'),
-            label: adapter.get('name'),
-          }),
-        ),
-      },
-      ...adapterProperties
-        .flatMap(property => {
-          const name = property.get('name');
-          return !property.get('sensitive') || isNew
-            ? [
-                {
-                  name: `property_${name}`,
-                  label: name,
-                  type: property.get('sensitive') ? 'password' : 'text',
-                  required: property.get('required'),
-                  transient: true,
-                  initialValue: getIn(bridge, ['properties', name], ''),
+const fields = ({ bridgeSlug, adapterClass }) => ({
+  bridge,
+  adapters,
+  adapterProperties,
+}) =>
+  adapterProperties && [
+    {
+      name: 'slug',
+      label: 'Bridge Slug',
+      type: 'text',
+      required: true,
+      initialValue: get(bridge, 'slug', ''),
+      helpText: 'Unique name used in the bridge path.',
+    },
+    {
+      name: 'adapterClass',
+      label: 'Adapter Class',
+      type: 'text',
+      enabled: false,
+      required: false,
+      initialValue: bridge ? bridge.get('adapterClass') : adapterClass,
+      options: adapters.map(adapter =>
+        Map({
+          value: adapter.get('class'),
+          label: adapter.get('name'),
+        }),
+      ),
+    },
+    ...adapterProperties
+      .flatMap(property => {
+        const name = property.get('name');
+        return !property.get('sensitive') || !bridge
+          ? [
+              {
+                name: `property_${name}`,
+                label: name,
+                type: property.get('sensitive') ? 'password' : 'text',
+                required: property.get('required'),
+                transient: true,
+                initialValue: getIn(bridge, ['properties', name], ''),
+              },
+            ]
+          : [
+              {
+                name: `property_${name}`,
+                label: name,
+                type: 'password',
+                required: property.get('required')
+                  ? ({ values }) => values.get(`changeProperty_${name}`)
+                  : false,
+                transient: true,
+                initialValue: getIn(bridge, ['properties', name], ''),
+                visible: ({ values }) => values.get(`changeProperty_${name}`),
+              },
+              {
+                name: `changeProperty_${name}`,
+                label: `Change ${name}`,
+                type: 'checkbox',
+                transient: true,
+                onChange: ({ values }, { setValue }) => {
+                  if (values.get(`property_${name}`) !== '') {
+                    setValue(`property_${name}`, '');
+                  }
                 },
-              ]
-            : [
-                {
-                  name: `property_${name}`,
-                  label: name,
-                  type: 'password',
-                  required: property.get('required')
-                    ? ({ values }) => values.get(`changeProperty_${name}`)
-                    : false,
-                  transient: true,
-                  initialValue: getIn(bridge, ['properties', name], ''),
-                  visible: ({ values }) => values.get(`changeProperty_${name}`),
-                },
-                {
-                  name: `changeProperty_${name}`,
-                  label: `Change ${name}`,
-                  type: 'checkbox',
-                  transient: true,
-                  onChange: ({ values }, { setValue }) => {
-                    if (values.get(`property_${name}`) !== '') {
-                      setValue(`property_${name}`, '');
-                    }
-                  },
-                },
-              ];
-        })
-        .toArray(),
-      {
-        name: 'properties',
-        visible: false,
-        initialValue: get(bridge, 'properties', {}),
-        serialize: ({ values }) =>
-          adapterProperties
-            .filter(
-              prop =>
-                isNew ||
-                !prop.get('sensitive') ||
-                values.get(`changeProperty_${prop.get('name')}`),
-            )
-            .map(prop => prop.get('name'))
-            .reduce(
-              (reduction, propName) =>
-                reduction.set(propName, values.get(`property_${propName}`)),
-              Map(),
-            )
-            .toObject(),
-      },
-    ];
-  }
-};
+              },
+            ];
+      })
+      .toArray(),
+    {
+      name: 'properties',
+      visible: false,
+      initialValue: get(bridge, 'properties', {}),
+      serialize: ({ values }) =>
+        adapterProperties
+          .filter(
+            prop =>
+              !bridge ||
+              !prop.get('sensitive') ||
+              values.get(`changeProperty_${prop.get('name')}`),
+          )
+          .map(prop => prop.get('name'))
+          .reduce(
+            (reduction, propName) =>
+              reduction.set(propName, values.get(`property_${propName}`)),
+            Map(),
+          )
+          .toObject(),
+    },
+  ];
 
 export const BridgeForm = generateForm({
   formOptions: ['bridgeSlug', 'adapterClass', 'agentSlug'],
