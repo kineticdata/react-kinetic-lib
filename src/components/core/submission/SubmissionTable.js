f6059c0fbe06298952373706b34270120a660e93
Shayne Koestler <shayne.koestler@kineticdata.com>
2020-05-20 11:09:23 -0500

    updated SubmissionTable to use Form for filtering KP-1128

--- src/components/core/submission/SubmissionTable.js
+++ src/components/core/submission/SubmissionTable.js
@@ -1,114 +1,51 @@
-import moment from 'moment';
-import { List } from 'immutable';
+import { getIn, List } from 'immutable';
 import { generateTable } from '../../table/Table';
-import { isValueEmpty } from '../../table/Table.redux';
 import {
+  fetchForm,
+  fetchKapp,
   searchSubmissions,
   SubmissionSearch,
-  VALID_DS_CORE_STATES,
   VALID_KAPP_CORE_STATES,
 } from '../../../apis';
 
-const processValues = (q, op, field, value, datastore) => {
-  if (op === 'startsWith' && datastore) {
-    return q.sw(field, value);
-  } else if (op === 'gt' && datastore) {
-    q.gt(field, value);
-  } else if (op === 'gteq' && datastore) {
-    q.gteq(field, value);
-  } else if (op === 'lt' && datastore) {
-    q.lt(field, value);
-  } else if (op === 'lteq' && datastore) {
-    q.lteq(field, value);
-  } else if (op === 'between' && datastore) {
-    q.between(field, value.get(0), value.get(1));
-  } else if (op === 'equals') {
-    return q.eq(field, value);
-  }
-  return q;
-};
+const applyMeta = (query, op, rvalue) => (rvalue ? query[op](rvalue) : query);
 
-const submissionsFilter = (paramData, props) => {
-  const { filters, pageSize } = paramData;
-  const { include = ['details', 'form'], datastore = false } = props;
-  const query = new SubmissionSearch(datastore);
-  const sortBy = paramData.sortColumn ? paramData.sortColumn : 'createdAt';
+const applyOp = (query, op, lvalue, rvalue) =>
+  rvalue ? query[op](lvalue, rvalue) : query;
 
-  query
-    .includes(include)
-    .sortBy(sortBy)
-    .sortDirection(paramData.sortDirection.toLocaleUpperCase())
+const submissionSearch = ({ filters, pageSize, sortColumn, sortDirection }) => {
+  const query = new SubmissionSearch()
+    .includes(['details', 'form'])
+    .sortBy(sortColumn || 'createdAt')
+    .sortDirection(sortDirection.toLocaleUpperCase())
     .limit(pageSize);
-
-  filters.reduce((q, filter) => {
-    const field = filter.getIn(['column', 'value']);
-    const op = filter.getIn(['column', 'filter'], 'equals');
-    const value = filter.get('value');
-
-    if (isValueEmpty(value)) {
-      return q;
-    } else if (field === 'coreState') {
-      q.coreState(value);
-    } else if (field === 'values') {
-      value.reduce(
-        (q, f) =>
-          processValues(
-            q,
-            'equals',
-            `values[${f.get('field')}]`,
-            f.get('value'),
-            datastore,
-          ),
-        q,
-      );
-    } else if (op === 'startsWith' && datastore) {
-      return q.sw(field, value);
-    } else if (op === 'gt' && datastore) {
-      q.gt(field, value);
-    } else if (op === 'gteq' && datastore) {
-      q.gteq(field, value);
-    } else if (op === 'lt' && datastore) {
-      q.lt(field, value);
-    } else if (op === 'lteq' && datastore) {
-      q.lteq(field, value);
-    } else if (op === 'between' && datastore) {
-      q.between(field, value.get(0), value.get(1));
-    } else if (op === 'between' && field === sortBy) {
-      const startDate = moment(value.get(0));
-      const endDate = moment(value.get(1));
-      if (startDate.isValid()) {
-        q.startDate(startDate.toDate());
-      }
-      if (endDate.isValid()) {
-        q.endDate(endDate.toDate());
-      }
-    } else if (op === 'in') {
-      q.in(field, value.toArray());
-    } else if (op === 'equals') {
-      return q.eq(field, value);
-    }
-    return q;
-  }, query);
-  return { search: query.build(), datastore };
+  applyMeta(query, 'coreState', filters.get('coreState'));
+  applyMeta(query, 'startDate', filters.get('startDate'));
+  applyMeta(query, 'endDate', filters.get('endDate'));
+  applyOp(query, 'eq', 'handle', filters.get('handle'));
+  applyOp(
+    query,
+    'eq',
+    'submittedBy',
+    filters.getIn(['submittedBy', 'username']),
+  );
+  filters
+    .get('values')
+    .forEach((value, field) => applyOp(query, 'eq', `values[${field}]`, value));
+  return query.build();
 };
 
-const dataSource = props => {
+const dataSource = ({ formSlug, kappSlug }) => {
   return {
     fn: searchSubmissions,
-    params: paramData => {
-      const formSlug = paramData.filters.getIn(['form', 'value'], '')
-        ? paramData.filters.getIn(['form', 'value'])
-        : props.formSlug;
-
-      return [
-        {
-          kapp: props.kappSlug ? props.kappSlug : null,
-          form: formSlug,
-          pageToken: paramData.nextPageToken,
-          ...submissionsFilter(paramData, props),
-        },
-      ];
-    },
+    params: paramData => [
+      {
+        kapp: kappSlug,
+        form: paramData.filters.getIn(['form', 'slug'], formSlug),
+        pageToken: paramData.nextPageToken,
+        search: submissionSearch(paramData),
+      },
+    ],
     transform: result => ({
       data: result.submissions,
       nextPageToken: result.nextPageToken,
@@ -125,45 +62,97 @@ const onValidateFilters = filters =>
       true,
     );
 
+const filterDataSources = ({ kappSlug, formSlug }) => ({
+  kapp: {
+    fn: fetchKapp,
+    params: [{ kappSlug, include: 'fields' }],
+    transform: result => result.kapp,
+  },
+  form: {
+    fn: formSlug =>
+      formSlug ? fetchForm({ kappSlug, formSlug, include: 'fields' }) : null,
+    params: ({ values }) => [getIn(values, ['form', 'slug'], formSlug)],
+    transform: result => result && result.form,
+  },
+  fieldOptions: {
+    fn: (form, kapp) =>
+      form ? form.get('fields').toArray() : kapp.get('fields').toArray(),
+    params: ({ form, kapp }) => kapp && [form, kapp],
+    transform: result =>
+      result.map(field => ({
+        label: field.get('name'),
+        value: field.get('name'),
+      })),
+  },
+  coreStateOptions: {
+    fn: () => VALID_KAPP_CORE_STATES,
+    params: [],
+    transform: coreStates =>
+      coreStates.map(coreState => ({ label: coreState, value: coreState })),
+  },
+});
+
+const filters = ({ kappSlug, formSlug }) => ({
+  coreStateOptions,
+  form,
+  forms,
+}) =>
+  coreStateOptions && [
+    { label: 'Start Date', name: 'startDate', type: 'text' },
+    { label: 'End Date', name: 'endDate', type: 'text' },
+    {
+      label: 'Handle',
+      name: 'handle',
+      pattern: /[A-F0-9]{6}/,
+      patternMessage:
+        'Handles only contain characters A-F and 0-9, and are exactly 6 characters long',
+      type: 'text',
+    },
+    !formSlug && {
+      label: 'Form',
+      name: 'form',
+      type: 'form',
+      search: { kappSlug },
+    },
+    { label: 'Submitted By', name: 'submittedBy', type: 'user' },
+    {
+      label: 'State',
+      name: 'coreState',
+      type: 'select',
+      options: coreStateOptions,
+    },
+    {
+      label: 'Values',
+      name: 'values',
+      type: 'map',
+      options: ({ fieldOptions }) => fieldOptions,
+    },
+  ];
+
 const columns = [
   {
     value: 'closedAt',
     title: 'Closed At',
-    filter: 'equals',
-    type: 'text',
     sortable: true,
   },
   {
     value: 'closedBy',
     title: 'closedBy',
-    filter: 'equals',
-    type: 'text',
     sortable: false,
   },
   {
     value: 'coreState',
     title: 'Core State',
-    filter: 'equals',
-    type: 'text',
-    options: ({ datastore }) =>
-      (datastore ? VALID_DS_CORE_STATES : VALID_KAPP_CORE_STATES).map(s => ({
-        value: s,
-        label: s,
-      })),
     sortable: false,
   },
   {
     value: 'createdAt',
     title: 'Created',
-    filter: 'equals',
-    type: 'text',
     sortable: true,
   },
   {
     value: 'createdBy',
     title: 'Created By',
-    filter: 'equals',
-    type: 'text',
     sortable: false,
   },
   {
@@ -204,48 +193,26 @@ const columns = [
   {
     value: 'submittedAt',
     title: 'Submitted At',
-    filter: 'equals',
-    type: 'text',
     sortable: true,
   },
   {
     value: 'submittedBy',
     title: 'Submitted By',
-    filter: 'equals',
-    type: 'text',
     sortable: false,
   },
   {
     value: 'type',
     title: 'Type',
-    filter: 'equals',
-    type: 'text',
     sortable: false,
   },
   {
     value: 'updatedAt',
     title: 'Updated',
-    filter: 'equals',
-    type: 'text',
     sortable: true,
   },
   {
     value: 'updatedBy',
     title: 'Updated By',
-    filter: 'equals',
-    type: 'text',
-    sortable: false,
-  },
-  // Used for filtering by values on the fly
-  {
-    value: 'values',
-    title: 'Values',
-    sortable: false,
-  },
-  // Used for filtering by form slug on the fly.
-  {
-    value: 'form',
-    title: 'Form',
     sortable: false,
   },
 ];
@@ -254,6 +221,8 @@ export const SubmissionTable = generateTable({
   tableOptions: ['kappSlug', 'formSlug', 'datastore'],
   columns,
   dataSource,
+  filterDataSources,
+  filters,
   onValidateFilters,
 });
 
